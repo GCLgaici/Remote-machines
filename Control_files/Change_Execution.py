64bc13eb453a9fbe4403bcd59b134e0b5edb59fb
GCLgaici <3471093372@qq.com>
2024-10-20 20:34:19 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -1,6 +1,5 @@
 """
-代码有变化就执行
+代码有变化就执行20241020
 """
-print('sb')
-self.main.wechat_push.Alleged_information(f'{self.main.sys_username}代码成功执行')
+self.main.wechat_push.Alleged_information(f'{self.main.sys_username}程序在线')
 
