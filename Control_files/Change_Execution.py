07f53a16e892f83cf969fb23dbb9f26d4b13c026
GCLgaici <3471093372@qq.com>
2024-10-20 23:35:56 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -1,10 +1,7 @@
 """
 代码有变化就执行2024-
 """
+# import subprocess
 
 
-
-
-
-self.main.wechat_push.Alleged_information(f'{self.main.sys_username}程序在线')
-
+self.main.wechat_push.Alleged_information(f'{self.main.sys_username}在')
