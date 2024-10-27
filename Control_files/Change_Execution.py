61aa104ea0395ebf8a0e54eec0b946b969cd6599
GCLgaici <3471093372@qq.com>
2024-10-27 21:35:43 +0800

    更新 Change_Execution.py

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -5,7 +5,7 @@ https://yc.052024.xyz/Control_files/Change_Execution.py
 """
 import os
 
-# 执行截图cx功能
-os.system('D:/Gaici/Remotely截屏.exe')
-self.main.wechat_push.Alleged_information(f'{self.main.sys_username}代码ok')
+# # 执行截图cx功能
+# os.system('D:/Gaici/Remotely截屏.exe')
+# self.main.wechat_push.Alleged_information(f'{self.main.sys_username}代码ok')
 
