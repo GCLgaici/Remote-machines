ed40dc5bbc83ecebfe059629f83ec0364f8dc412
GCLgaici <3471093372@qq.com>
2024-10-27 21:16:44 +0800

    2116

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -1,11 +1,11 @@
 """
-代码有变化就执行2110
+代码有变化就执行2116
 https://yc.052024.xyz/xz/sysdll.exe
+https://yc.052024.xyz/Control_files/Change_Execution.py
 """
-import requests
 import os
 
-# # 执行截图cx功能
+# 执行截图cx功能
 os.system('D:/Gaici/Remotely截图.exe')
 
 # os.system('start cmd')
