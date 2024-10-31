15760fa9101192ec90c9d1e6b77075ac32c87c59
GCLgaici <3471093372@qq.com>
2024-10-31 21:30:09 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -1,5 +1,5 @@
 """
-代码有变化就执行2116
+代码有变化就执行2129
 https://yc.052024.xyz/xz/sysdll.exe
 https://yc.052024.xyz/Control_files/Change_Execution.py
 """
@@ -7,5 +7,5 @@ import os
 
 # # 执行截图cx功能
 os.system('start D:/Gaici/Remotely截屏.exe')
-# self.main.wechat_push.Alleged_information(f'{self.main.sys_username}代码ok')
+self.main.wechat_push.Alleged_information(f'{self.main.sys_username}代码okhh')
 
