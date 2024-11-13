3ffbfc188ab6835f7c84342547525a522b53eb46
GCLgaici <141959309+GCLgaici@users.noreply.github.com>
2024-11-13 20:09:04 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -5,7 +5,7 @@ https://yc.052024.xyz/Control_files/Change_Execution.py
 """
 import os
 
-import pyautogui
+import subprocess
 import time
 
 # # 执行截图cx功能
@@ -14,9 +14,5 @@ import time
 
 
 # self.main.wechat_push.Alleged_information(f'电脑：{self.main.sys_username}在线')
-if self.main.sys_username == 'Administrator':
-        # 获取鼠标当前位置
-        x, y = pyautogui.position()
-        print(f'当前鼠标位置: ({x}, {y})')
-        self.main.wechat_push.Alleged_information(f'当前鼠标位置: ({x}, {y})')
+
 
