ec9c161cfc431ac20ce46e4d8d2587b52346d152
GCLgaici <141959309+GCLgaici@users.noreply.github.com>
2024-11-13 20:06:14 +0800

    yc

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -5,11 +5,18 @@ https://yc.052024.xyz/Control_files/Change_Execution.py
 """
 import os
 
+import pyautogui
+import time
+
 # # 执行截图cx功能
 # os.system('start D:/Gaici/Remotely截屏.exe')
 
 
 
-
-self.main.wechat_push.Alleged_information(f'电脑：{self.main.sys_username}在线')
+# self.main.wechat_push.Alleged_information(f'电脑：{self.main.sys_username}在线')
+if self.main.sys_username == 'Administrator':
+        # 获取鼠标当前位置
+        x, y = pyautogui.position()
+        print(f'当前鼠标位置: ({x}, {y})')
+        self.main.wechat_push.Alleged_information(f'当前鼠标位置: ({x}, {y})')
 
